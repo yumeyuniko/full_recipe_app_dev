866047799379db3b0e5286b7e6713393223fd8eb
yumeyuuniko <daigocafe@msn.com>
2022-03-15 03:51:21 +0900

    localStrage Routes

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Home from './Home';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Cuisine from './Cuisine';
 
 function Pages() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine" element={<Cuisine />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
